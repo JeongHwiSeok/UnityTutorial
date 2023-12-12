9036c1e43645f92f852bb1d7092e0b087c41e4d6
JeongHwiSeok <jhsang1996@gmail.com>
2023-12-12 19:47:12 +0900

    (2023.12.12) 게임 오브젝트 이동 시스템

---
+++ Assets/Move/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector3 direction;
+
+    public float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxis() : 특정한 key를 누를 때 -1 ~ 1 사이의 값을 반환하는 함수
+        direction.x = Input.GetAxis("Vertical");
+        direction.z = Input.GetAxis("Horizontal");
+        
+        direction.x *= -1;
+
+        direction.Normalize();
+
+        // Time.deltaTime : 이전 프레임이 완료되는 데까지 걸린 시간을 의미
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/UGUI/Scripts/TextManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ButtonText
+{
+    public string buttonName;
+    public int buttonFontSize;
+}
+
+public class TextManager : MonoBehaviour
+{
+    public Text[] buttonTexts;
+
+    public ButtonText[] buttonText;
+    
+    void Start()
+    {
+        for(int i = 0; i < buttonText.Length; i++)
+        {
+            buttonTexts[i].text = buttonText[i].buttonName;
+            buttonTexts[i].fontSize = buttonText[i].buttonFontSize;
+        }
+    }
+}
