31996ae6f0e8a9cb7bd4b858d270a3be63a8e297
JeongHwiSeok <jhsang1996@gmail.com>
2023-12-19 19:47:19 +0900

    (2023.12.19) 코루틴

--- Assets/Instantiate/Scripts/SpawnManager.cs
+++ Assets/Instantiate/Scripts/SpawnManager.cs
@@ -4,30 +4,62 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] GameObject unit;
-    [SerializeField] Transform createPosition;
+    [SerializeField] List<Unit> listUnits;
 
-    [Tooltip("몬스터를 생성하는 변수")]
-    [SerializeField] int spawnMax = 5;
+    [SerializeField] Factory factory;
+
     public Vector3 direction;
+
+    float timer = 0;
 
     private void Start()
     {
         direction.x = 0;
         // Instantiate : 게임 오브젝트를 생성하는 함수
-        for(int i = 0; i < spawnMax; i++)
+        #region for문을 이용한 몬스터 스폰
+        //for(int i = 0; i < spawnMax; i++)
+        //{
+        //    // 1. 게임 오브젝트를 생성
+        //    GameObject monster = Instantiate(unit, createPosition);
+
+        //    // 2. 생성된 게임 오브젝트의 위치를 설정
+        //    // monster.transform.position += direction;
+        //    monster.transform.position = new Vector3(i * 5, 0, createPosition.position.z);
+
+        //    Debug.Log("World Position : " + monster.transform.position);
+        //    Debug.Log("Local Position : " + monster.transform.localPosition);
+
+        //    direction.x += 5;      
+        //}
+        #endregion
+
+        StartCoroutine(CreateRoutine());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            // 1. 게임 오브젝트를 생성
-            GameObject monster = Instantiate(unit, createPosition);
-
-            // 2. 생성된 게임 오브젝트의 위치를 설정
-            // monster.transform.position += direction;
-            monster.transform.position = new Vector3(i * 5, 0, createPosition.position.z);
-
-            Debug.Log("World Position : " + monster.transform.position);
-            Debug.Log("Local Position : " + monster.transform.localPosition);
-
-            direction.x += 5;      
+            StartCoroutine(LogRoutine());
         }
+    }
+
+    public IEnumerator CreateRoutine()
+    {
+        while(true)
+        {
+            // Random.Range(0, n) : 0 ~ n-1 까지의 값을 반환
+            factory.CreateUnit(listUnits[Random.Range(0, listUnits.Count)]);
+
+            // new WaitForSeconds() : 특정한 시간동안 코루틴을 대기
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public IEnumerator LogRoutine()
+    {
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("Attack");
     }
 }
