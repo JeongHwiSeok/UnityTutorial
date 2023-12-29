ec7e02325b074c2138597d3eab5fbe39cee6d701
JeongHwiSeok <jhsang1996@gmail.com>
2023-12-29 19:48:30 +0900

    (2023.12.29) 물리 재질 적용

--- Assets/Character Select/Script/CharacterSelectManager.cs
+++ Assets/Character Select/Script/CharacterSelectManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CharacterSelectManager : MonoBehaviour
 {
-    [SerializeField] int selectCount = 0;
     [SerializeField] List<GameObject> characterList;
 
     // Start is called before the first frame update
@@ -22,6 +21,34 @@ public class CharacterSelectManager : MonoBehaviour
             characterList[i].SetActive(false);
         }
 
-        characterList[selectCount].SetActive(true);
+        characterList[DataManager.instance.SelectCount].SetActive(true);
+        characterList[DataManager.instance.SelectCount].transform.position = new Vector3(0, 0, 0);
+        characterList[DataManager.instance.SelectCount].transform.rotation = new Quaternion(0, 0, 0, 0);
+    }
+    
+    public void OnLeftButton()
+    {
+        DataManager.instance.SelectCount--;
+
+        if(DataManager.instance.SelectCount < 0)
+        {
+            DataManager.instance.SelectCount = characterList.Count - 1;
+        }
+
+        Show();
+        DataManager.instance.Save();
+    }
+
+    public void OnRightButton()
+    {
+        DataManager.instance.SelectCount++;
+
+        if(DataManager.instance.SelectCount >= characterList.Count)
+        {
+            DataManager.instance.SelectCount = 0;
+        }
+
+        Show();
+        DataManager.instance.Save();
     }
 }
